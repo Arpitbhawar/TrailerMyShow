855a3de76cf36d8ebfa112ef88acc55d43e063df
Arpit <arpit@arpit-PC.(none)>
2015-06-22 19:55:58 +0530

    optimising code to get to reduce the number the iteration for same movie

--- Source/MainSource.py
+++ Source/MainSource.py
@@ -13,7 +13,8 @@ DEVELOPER_KEY = "AIzaSyCndc_YwoZgZjGRHltV-GzydtaVTKjyKxI"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
-cities = ["mumbai","bangalore","pune","goa","indore","ahmedabad","chandigarh","chennai","kolkata","mysore"]
+""" cities = ['agartala', 'agra', 'ahmedabad', 'ajmer', 'aligarh', 'allahabad', 'amritsar', 'aurangabad', 'bangalore', 'bhopal', 'bilaspur', 'chandigarh', 'chennai', 'coimbatore', 'dehradun', 'durgapur', 'faridabad', 'goa', 'guwahati', 'gwalior', 'haridwar', 'hyderabad', 'indore', 'jabalpur', 'jaipur', 'jamnagar', 'jamshedpur', 'jodhpur', 'kanpur', 'kolhapur', 'kolkata', 'kota', 'lucknow', 'ludhiana', 'madurai', 'mangalore', 'meerut', 'mumbai', 'muzaffarnagar', 'mysore', 'narpur', 'nashik', 'patiala', 'patna', 'pune', 'raipur', 'rajkot', 'ranchi', 'rohtak', 'solapur', 'surat', 'udaipur', 'ujjain', 'vadodara', 'varanasi'] """
+cities=['goa']
 distinct_movies = set()
 movie_dict = dict()
 movies_with_id = dict()
@@ -74,13 +75,49 @@ def YouTube():
         (options, args) = parser.parse_args()
         VideoId=youtube_search(options)
         movies_with_id[m]=VideoId
-        if VideoId:
-            print m+" : "+VideoId
-        
-def IMDBRating():
+        #if VideoId:
+        #    print m+" : "+VideoId
+
+lock = threading.Lock()
+def updateSharedDict(movie,rating,duration,genre):
+    with lock:
+        movies_with_rating[movie]=(rating,duration,genre)
+    
+def IMDBRating1():
     baseurl="http://www.imdb.com"
-    for m in distinct_movies:
-        nameurl = "/find?s=tt&q="+m
+    length = len(distinct_movies)
+    distinct_movies_list = list(distinct_movies)
+    for m in range(length//2):
+        nameurl = "/find?s=tt&q="+distinct_movies_list[m]
+        response = ul.urlopen(baseurl+nameurl)
+        namePageSoup = BeautifulSoup(response.read())
+        rating_link = namePageSoup.table.a['href']
+        response2 = ul.urlopen(baseurl+rating_link)
+        ratingPageSoup = BeautifulSoup(response2.read())
+        rating=''
+        duration=''
+        genre=''
+        try:
+            rating = ratingPageSoup.findAll("div", { "class" : "star-box-details" })[0].span.contents[0]
+            duration = ratingPageSoup.findAll("div", { "class" : "infobar" })[0].time.contents[0]
+            duration = duration.strip()
+            allGenreItems = ratingPageSoup.findAll("span", {"itemprop" : "genre"})
+            genre = allGenreItems[0].contents[0]
+            for g in range(1,len(allGenreItems)):
+                genre+= '|'+ allGenreItems[g].contents[0]
+            
+        except:
+            rating = "0"
+        updateSharedDict(distinct_movies_list[m],rating,duration,genre)
+        #print distinct_movies_list[m]+" -> "+rating+" -> "+duration + " -> " +genre
+    
+
+def IMDBRating2():
+    baseurl="http://www.imdb.com"
+    length = len(distinct_movies)
+    distinct_movies_list = list(distinct_movies)
+    for m in range(length//2,length):
+        nameurl = "/find?s=tt&q="+distinct_movies_list[m]
         response = ul.urlopen(baseurl+nameurl)
         namePageSoup = BeautifulSoup(response.read())
         rating_link = namePageSoup.table.a['href']
@@ -98,27 +135,37 @@ def IMDBRating():
                 genre+= '|'+ allGenreItems[g].contents[0]
             
         except:
-            rating = "N/A"
-        movies_with_rating[m]=(rating,duration,genre)
-        print m+" -> "+rating+" -> "+duration + " -> " +genre
-    
+            rating = "0"
+        updateSharedDict(distinct_movies_list[m],rating,duration,genre)
+        #print distinct_movies_list[m]+" -> "+rating+" -> "+duration + " -> " +genre
+
 
 if __name__ == "__main__":
     start_time = time.time()
     BookMyShow()
     thread1 = threading.Thread(target=YouTube,args=())
-    thread2 = threading.Thread(target=IMDBRating,args=())
+    thread2 = threading.Thread(target=IMDBRating1,args=())
+    thread3 = threading.Thread(target=IMDBRating2,args=())
+    thread1.daemon=True
+    thread2.daemon=True
+    thread3.daemon=True
     thread1.start()
     thread2.start()
+    thread3.start()
     thread1.join()
     thread2.join()
+    thread3.join()
     path="../Output/"
     for city in movie_dict:
         outfile=open(path+city+".json","w")
         IntermediateOutputString="{"
         for movie in movie_dict[city]:
             if movies_with_id[movie]:
-                 IntermediateOutputString+='\"' + movie + '\":[' + '\"' + movies_with_id[movie].strip() + '\",' + '\"' + movies_with_rating[movie][0] +'\",'+'\"'+movies_with_rating[movie][1]+'\",'+'\"'+movies_with_rating[movie][2] +'\"],'
+                try:
+                    IntermediateOutputString+='\"' + movie + '\":[' + '\"' + movies_with_id[movie].strip() + '\",' + '\"' + movies_with_rating[movie][0] +'\",'+'\"'+movies_with_rating[movie][1]+'\",'+'\"'+movies_with_rating[movie][2] +'\"],'
+                except:
+                    IntermediateOutputString+='\"' + movie + '\":[' + '\"' + movies_with_id[movie].strip() + '\",' + '\"' + '' +'\",'+'\"'+ '' +'\",'+'\"'+ '' +'\"],'
+                    #Just to be safe and handle key errors    
 
         IntermediateOutputString= IntermediateOutputString[0:-1] + "}"
         JsonOutputString = json.loads(IntermediateOutputString)
