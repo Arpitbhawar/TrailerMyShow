34d3490f13676fe92730f3e42b2a8f1f5219376f
Arpit <arpit@arpit-PC.(none)>
2015-06-24 01:04:56 +0530

    used set to improve the performance

--- Source/MainSource.py
+++ Source/MainSource.py
@@ -9,23 +9,25 @@ import os
 # Set DEVELOPER_KEY to the "API key" value from the "Access" tab of the
 # Google APIs Console http://code.google.com/apis/console#access
 # Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = "AIzaSyCndc_YwoZgZjGRHltV-GzydtaVTKjyKxI"
+DEVELOPER_KEY = "AIzaSyA_NDpr8L3Ie42WH6ZjDeKu8UBu1pb2EHg" #old "AIzaSyCndc_YwoZgZjGRHltV-GzydtaVTKjyKxI"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
-""" cities = ['agartala', 'agra', 'ahmedabad', 'ajmer', 'aligarh', 'allahabad', 'amritsar', 'aurangabad', 'bangalore', 'bhopal', 'bilaspur', 'chandigarh', 'chennai', 'coimbatore', 'dehradun', 'durgapur', 'faridabad', 'goa', 'guwahati', 'gwalior', 'haridwar', 'hyderabad', 'indore', 'jabalpur', 'jaipur', 'jamnagar', 'jamshedpur', 'jodhpur', 'kanpur', 'kolhapur', 'kolkata', 'kota', 'lucknow', 'ludhiana', 'madurai', 'mangalore', 'meerut', 'mumbai', 'muzaffarnagar', 'mysore', 'narpur', 'nashik', 'patiala', 'patna', 'pune', 'raipur', 'rajkot', 'ranchi', 'rohtak', 'solapur', 'surat', 'udaipur', 'ujjain', 'vadodara', 'varanasi'] """
-cities=['goa']
+cities = ['Agartala', 'Agra', 'Ahmedabad', 'Ajmer', 'Aligarh', 'Allahabad', 'Amritsar', 'Aurangabad', 'Bengaluru', 'Bhopal', 'Bilaspur', 'Chandigarh', 'Chennai', 'Coimbatore', 'Dehradun', 'Durgapur', 'Goa', 'Guwahati', 'Gwalior', 'Haridwar', 'Hyderabad', 'Indore', 'Jabalpur', 'Jaipur', 'Jamnagar', 'Jamshedpur', 'Jodhpur', 'Kanpur', 'Kolhapur', 'Kolkata', 'Kota', 'Lucknow', 'Ludhiana', 'Madurai', 'Mangalore', 'Meerut', 'Mumbai', 'Muzaffarnagar', 'Mysore', 'Nagpur', 'Nashik', 'Patiala', 'Patna', 'Pune', 'Raipur', 'Rajkot', 'Ranchi', 'Rohtak', 'Solapur', 'Surat', 'Udaipur', 'Ujjain', 'Vadodara']
+#cities=['Agra']
+#cities=['faridabad']
 distinct_movies = set()
 movie_dict = dict()
 movies_with_id = dict()
 movies_with_rating=dict()
+movies_with_lang=dict()
 
 def BookMyShow():
     for i in cities:
         try:
             j=i.encode('utf-8')
             url_str="http://in.bookmyshow.com/"+j+"/movies"
-            
+            count=0
             print("trying to open link "+url_str)
             response = ul.urlopen(url_str)
             page_content = response.read()
@@ -40,6 +42,13 @@ def BookMyShow():
                     idx = var_movie.find('(')
                     if idx !=-1:
                         var_movie = var_movie[0:idx-1]
+                    soup_lang=((soup.findAll("span", { "class" : "language" })[count]['title']).encode('utf-8')).strip()
+                    count=count+1
+                    #print("soup_lang "+soup_lang)
+                    idx = soup_lang.find(' ')
+                    if idx !=-1:
+                        soup_lang = soup_lang[0:idx]
+                    movies_with_lang[var_movie]=soup_lang
                     movies.add(var_movie)
                     distinct_movies.add(var_movie)
                     
@@ -67,8 +76,8 @@ def youtube_search(options):
 def YouTube():
     parser = OptionParser()
     for m in distinct_movies:
-        toSearch=m+" official trailer"
-        toSearch=m.encode('utf-8')
+        toSearch=(m+" official trailer").encode('utf-8')
+        
         parser = OptionParser(conflict_handler="resolve")
         parser.add_option("--q", dest="q", help="Search term",default=toSearch)
         parser.add_option("--max-results", dest="maxResults",help="Max results", default=1)
@@ -79,9 +88,9 @@ def YouTube():
         #    print m+" : "+VideoId
 
 lock = threading.Lock()
-def updateSharedDict(movie,rating,duration,genre):
+def updateSharedDict(movie,rating,duration,genre,language):
     with lock:
-        movies_with_rating[movie]=(rating,duration,genre)
+        movies_with_rating[movie]=(rating,duration,genre,language)
     
 def IMDBRating1():
     baseurl="http://www.imdb.com"
@@ -97,6 +106,7 @@ def IMDBRating1():
         rating=''
         duration=''
         genre=''
+        lang=''
         try:
             rating = ratingPageSoup.findAll("div", { "class" : "star-box-details" })[0].span.contents[0]
             duration = ratingPageSoup.findAll("div", { "class" : "infobar" })[0].time.contents[0]
@@ -105,10 +115,17 @@ def IMDBRating1():
             genre = allGenreItems[0].contents[0]
             for g in range(1,len(allGenreItems)):
                 genre+= '|'+ allGenreItems[g].contents[0]
-            
         except:
             rating = "0"
-        updateSharedDict(distinct_movies_list[m],rating,duration,genre)
+        temp_movie=distinct_movies_list[m]
+        try:
+            lang=movies_with_lang[temp_movie]
+
+            print "temp_movie ->"+temp_movie+" language "+lang
+        except:
+            #print "inside exception for language for "+temp_movie
+            print "temp_movie ->"+temp_movie+" language "+lang
+        updateSharedDict(distinct_movies_list[m],rating,duration,genre,lang)
         #print distinct_movies_list[m]+" -> "+rating+" -> "+duration + " -> " +genre
     
 
@@ -125,6 +142,8 @@ def IMDBRating2():
         ratingPageSoup = BeautifulSoup(response2.read())
         rating=''
         duration=''
+        genre=''
+        lang=''
         try:
             rating = ratingPageSoup.findAll("div", { "class" : "star-box-details" })[0].span.contents[0]
             duration = ratingPageSoup.findAll("div", { "class" : "infobar" })[0].time.contents[0]
@@ -133,10 +152,16 @@ def IMDBRating2():
             genre = allGenreItems[0].contents[0]
             for g in range(1,len(allGenreItems)):
                 genre+= '|'+ allGenreItems[g].contents[0]
-            
         except:
             rating = "0"
-        updateSharedDict(distinct_movies_list[m],rating,duration,genre)
+        temp_movie=distinct_movies_list[m]
+        try:
+            lang=movies_with_lang[temp_movie]
+            print "temp_movie ->"+temp_movie+" language "+lang
+        except:
+            #print "inside exception for language for "+temp_movie
+            print "temp_movie ->"+temp_movie+" language "+lang
+        updateSharedDict(distinct_movies_list[m],rating,duration,genre,lang)
         #print distinct_movies_list[m]+" -> "+rating+" -> "+duration + " -> " +genre
 
 
@@ -155,16 +180,17 @@ if __name__ == "__main__":
     thread1.join()
     thread2.join()
     thread3.join()
-    path="../Output/"
+    path="../Main_Website/Output/"
     for city in movie_dict:
         outfile=open(path+city+".json","w")
         IntermediateOutputString="{"
         for movie in movie_dict[city]:
             if movies_with_id[movie]:
                 try:
-                    IntermediateOutputString+='\"' + movie + '\":[' + '\"' + movies_with_id[movie].strip() + '\",' + '\"' + movies_with_rating[movie][0] +'\",'+'\"'+movies_with_rating[movie][1]+'\",'+'\"'+movies_with_rating[movie][2] +'\"],'
+                    IntermediateOutputString+='\"' + movie + '\":[' + '\"' + movies_with_id[movie].strip() + '\",' + '\"' + movies_with_rating[movie][0] +'\",'+'\"'+movies_with_rating[movie][1]+'\",'+'\"'+movies_with_rating[movie][2] +'\",'+'\"'+movies_with_rating[movie][3]+'\"],'
                 except:
-                    IntermediateOutputString+='\"' + movie + '\":[' + '\"' + movies_with_id[movie].strip() + '\",' + '\"' + '' +'\",'+'\"'+ '' +'\",'+'\"'+ '' +'\"],'
+                    IntermediateOutputString+='\"' + movie + '\":[' + '\"' + movies_with_id[movie].strip() + '\",' + '\"' + '' +'\",'+'\"'+ '' +'\",'+'\"'+ ''+'\",'+'\"'+'' +'\"],'
+                    #print movie " -> " + movies_with_id[movie].strip() +" -> "+movies_with_rating[movie][0]+" -> "+movies_with_rating[movie][1]+" -> "+movies_with_rating[movie][2]+" -> "+movies_with_rating[movie][3]
                     #Just to be safe and handle key errors    
 
         IntermediateOutputString= IntermediateOutputString[0:-1] + "}"
